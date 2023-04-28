95b4458ea296769fa9bac63830231d58f18cb33f
adi1999 <adityadhookia@gmail.com>
2023-04-28 16:31:12 +0530

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PropertiesComponent } from './properties/properties.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PropertiesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
